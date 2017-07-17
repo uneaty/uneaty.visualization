847469bf9d4b44f325ce04d778585b534b548449
Sam Alston <samalsto@amazon.com>
2017-07-16 18:29:42 -0700

    Adding simple UX to run.

---
+++ Assets/Scripts/EvolutionUI.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class EvolutionUI : MonoBehaviour
+{
+    public Optimizer Optimizer;
+
+    void OnGUI()
+    {
+        if (GUI.Button(new Rect(10, 10, 100, 40), "Start EA"))
+        {
+            Optimizer.StartEA();
+        }
+        if (GUI.Button(new Rect(10, 60, 100, 40), "Stop EA"))
+        {
+            Optimizer.StopEA();
+        }
+        if (GUI.Button(new Rect(10, 110, 100, 40), "Run Best"))
+        {
+            Optimizer.RunBest();
+        }
+        if (GUI.Button(new Rect(10, 160, 100, 40), "Reset"))
+        {
+            Optimizer.Reset();
+        }
+
+        GUI.Button(new Rect(10, Screen.height - 70, 100, 60),
+            string.Format("Generation: {0}\nFitness: {1:0.00000000}", Optimizer.Generation, Optimizer.Fitness));
+    }
+}
